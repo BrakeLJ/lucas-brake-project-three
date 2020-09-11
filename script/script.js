00ea28c1b7c03fcea382e6c0d34388c2d920a98a
BrakeLJ <lukejbrake@gmail.com>
2020-09-11 09:07:28 -0400

    logic to play  ��sound on click

--- script/script.js
+++ script/script.js
@@ -1,68 +1,24 @@
 const app = {};
 
 app.init = () => {
-    $('.mixer').on('keydown', function(e) {
-        const keyPress = (`audio[data-key="${e.keyCode}"]`)
+    //Stores keycode when  button pressed and plays audio sample assosiated with it
+    $('.mixer__btn').on('keydown', function(event) {
+        const keyPress = (`audio[data-key="${event.keyCode}"]`);
         $(keyPress)[0].currentTime = 0;
         $(keyPress)[0].play();
-        //add class to handle transition
     });
 
-    $('.mixer').on('click', function() {
-
+    //stores keycode when button clicked and plays audio sample assosiated with it
+    $('.mixer__btn').on('click', function(e) {
+        const getKeyCode = $(this).attr('data-key');
+        const buttonClick = (`audio[data-key="${getKeyCode}"]`);
+        $(buttonClick)[0].currentTime = 0; 
+        $(buttonClick)[0].play();
     })
-
-
-    // $('.mixer__btn2').on('click', function () {
-    //     $('audio')[1].play();
-    // });
-    // $('.mixer__btn3').on('click', function () {
-    //     $('audio')[2].play();
-    // });
-    // $('.mixer__btn4').on('click', function () {
-    //     $('audio')[3].play();
-    // });
-    // $('.mixer__btn5').on('click', function () {
-    //     $('audio')[4].play();
-    // });
-    // $('.mixer__btn6').on('click', function () {
-    //     $('audio')[5].play();
-    // });
-    // $('.mixer__btn7').on('click', function () {
-    //     $('audio')[6].play();
-    // });
-    // $('.mixer__btn8').on('click', function () {
-    //     $('audio')[7].play();
-    // });
-    // $('.mixer__btn9').on('click', function () {
-    //     $('audio')[8].play();
-    // });
-    // $('.mixer__btn10').on('click', function () {
-    //     $('audio')[9].play();
-    // });
-    // $('.mixer__btn11').on('click', function () {
-    //     $('audio')[10].play();
-    // });
-    // $('.mixer__btn12').on('click', function () {
-    //     $('audio')[11].play();
-    // });
-    // $('.mixer__btn13').on('click', function () {
-    //     $('audio')[12].play();
-    // });
-    // $('.mixer__btn14').on('click', function () {
-    //     $('audio')[13].play();
-    // });
-    // $('.mixer__btn15').on('click', function () {
-    //     $('audio')[14].play();
-    // });
-    // $('.mixer__btn16').on('click', function () {
-    //     $('audio')[15].play();
-    // });
 }
 
 $(function(){
     app.init();
     });
 
-    //write a function that stores the value of a key being pressed
-    //when the key is clicked it stores the value and plays that key in the array
+    
